ba59ba439de69aeddfa34339211c4854b5c1c95b
gerizim16 <gerizim16@gmail.com>
2021-03-18 02:33:37 +0800

    Added initial classes

--- hud.py
+++ hud.py
@@ -4,23 +4,55 @@ from pyglet import font
 
 # Note that the font, font size, and positions of labels are still subject to change
 
-font.add_file('assets/DUMMY-FONT.ttf')
-new_font = font.load('DUMMY FONT', 16)
+# font.add_file('assets/DUMMY-FONT.ttf')
+# new_font = font.load('DUMMY FONT', 16)
 
 # Main Menu / PHASE 0
-label_title_1 = pyglet.text.Label("", font_name='DUMMY FONT', font_size=30, anchor_x='center', x=640, y=480)
-label_title_2 = pyglet.text.Label("", font_name='DUMMY FONT', font_size=22.5, anchor_x='center', x=640, y=445)
-label_names = pyglet.text.Label("Menina · Villarante · Virtucio", font_name='DUMMY FONT', font_size=10, anchor_x='center', x=640, y=407)
+label_title_1 = pyglet.text.Label("",
+                                  font_name='DUMMY FONT',
+                                  font_size=30,
+                                  anchor_x='center',
+                                  x=640,
+                                  y=480)
+label_title_2 = pyglet.text.Label("",
+                                  font_name='DUMMY FONT',
+                                  font_size=22.5,
+                                  anchor_x='center',
+                                  x=640,
+                                  y=445)
+label_names = pyglet.text.Label("Menina · Villarante · Virtucio",
+                                font_name='DUMMY FONT',
+                                font_size=10,
+                                anchor_x='center',
+                                x=640,
+                                y=407)
 
 label_button = []
-label_button.append(pyglet.text.Label("NEW GAME", font_name='DUMMY FONT', font_size=10, anchor_x='center', x=640, y=407))
-label_button.append(pyglet.text.Label("LOAD SAVED", font_name='DUMMY FONT', font_size=10, anchor_x='center', x=640, y=407))
-label_button.append(pyglet.text.Label("LOAD QUICKSAVE", font_name='DUMMY FONT', font_size=10, anchor_x='center', x=640, y=407))
+label_button.append(
+    pyglet.text.Label("NEW GAME",
+                      font_name='DUMMY FONT',
+                      font_size=10,
+                      anchor_x='center',
+                      x=640,
+                      y=407))
+label_button.append(
+    pyglet.text.Label("LOAD SAVED",
+                      font_name='DUMMY FONT',
+                      font_size=10,
+                      anchor_x='center',
+                      x=640,
+                      y=407))
+label_button.append(
+    pyglet.text.Label("LOAD QUICKSAVE",
+                      font_name='DUMMY FONT',
+                      font_size=10,
+                      anchor_x='center',
+                      x=640,
+                      y=407))
 
 # In Game / PHASE 1
 
 # Pause Menu / PHASE 2
-
 
 
 def draw(phase):
@@ -31,5 +63,60 @@ def draw(phase):
 
         for i in label_button:
             i.draw()
-    
+
     elif phase == 2:
+        pass
+
+
+class Button(object):
+    def __init__(self,
+                 text='',
+                 font_name=None,
+                 font_size=None,
+                 color=(255, 255, 255, 255),
+                 bg_color=(0, 0, 0),
+                 x=0,
+                 y=0,
+                 width=None,
+                 height=None,
+                 align='left',
+                 multiline=False,
+                 batch=None,
+                 func=None):
+        self.items = []
+        self.func = func
+
+        self.bg = pyglet.shapes.Rectangle(
+            x,
+            y,
+            0,
+            0,
+            color=bg_color,
+            batch=batch,
+            group=pyglet.graphics.OrderedGroup(1))
+
+        self.items.append(self.bg)
+        self.label = pyglet.text.Label(text,
+                                       x=x,
+                                       y=y,
+                                       color=color,
+                                       anchor_x='center',
+                                       anchor_y='center',
+                                       align=align,
+                                       multiline=multiline,
+                                       batch=batch,
+                                       group=pyglet.graphics.OrderedGroup(2))
+        self.items.append(self.label)
+
+        if width is None:
+            width = self.label.content_width + 40
+        if height is None:
+            height = self.label.content_height + 30
+        self.bg.width = width
+        self.bg.height = height
+        self.bg.anchor_x = width / 2
+        self.bg.anchor_y = height / 2
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if self.func and self.bg.x - self.bg.width / 2 <= x <= self.bg.x + self.bg.width / 2 and self.bg.y - self.bg.height / 2 <= y <= self.bg.y + self.bg.height / 2:
+            self.func()
--- main.py
+++ main.py
@@ -1,34 +1,152 @@
+from enum import Enum
+
+# project modules
+import hud
+
 # third party modules
 import pyglet
 
+# constants
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+MAIN_MENU, IN_GAME, PAUSE_MENU = range(3)
 
-class MainWindow(pyglet.window.Window):
+
+class Game(object):
     def __init__(self, width, height, caption="", resizeable=False):
-        super().__init__(width, height, caption, resizeable)
-        self.main_batch = pyglet.graphics.Batch()
+        self.window = pyglet.window.Window(width, height, caption, resizeable)
+
+        # set clear color to white
+        pyglet.gl.glClearColor(1, 1, 1, 1)
+
+        # initiate phases
+        self.phases = {
+            MAIN_MENU: MainMenu(self),
+            IN_GAME: InGame(self),
+            PAUSE_MENU: PauseMenu(self),
+        }
+
+        # set current phase to main menu
+        self.cur_phase = self.phases[MAIN_MENU]
+        self.window.push_handlers(self.cur_phase.on_draw,
+                                  self.cur_phase.on_key_press,
+                                  self.cur_phase.on_mouse_press)
 
         pyglet.clock.schedule_interval(self.update, 1 / 60.0)
 
-    def on_draw(self):
-        '''
-        GUI updates here
-        '''
-        # self.clear()
-        self.main_batch.draw()
+    def change_phase(self, phase):
+        self.window.pop_handlers()
+        self.cur_phase = self.phases[phase]
+        self.window.push_handlers(self.cur_phase.on_draw,
+                                  self.cur_phase.on_key_press,
+                                  self.cur_phase.on_mouse_press)
 
     def update(self, dt):
-        '''
-        logic updates here
-        '''
+        self.cur_phase.update(dt)
+
+
+class Phase(object):
+    '''
+    abstract class
+    '''
+    def __init__(self, game):
+        self.game = game
+
+    def on_draw(self):
         pass
 
-    def on_mouse_press(self, x, y, button, modifier):
-        print("Mouse pressed:", x, y, button, modifier)
+    def on_key_press(self, symbol, modifiers):
+        print(symbol, modifiers)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        print(x, y, button, modifiers)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def reset(self):
+        pass
+
+
+class MainMenu(Phase):
+    def __init__(self, game):
+        super().__init__(game)
+        self.batch = pyglet.graphics.Batch()
+        self.clickables = []  # list of clickable objects
+
+        pyglet.text.Label("Menina · Villarante · Virtucio",
+                          font_name='DUMMY FONT',
+                          font_size=10,
+                          color=(0, 0, 0, 255),
+                          anchor_x='center',
+                          x=SCREEN_WIDTH // 2,
+                          y=200,
+                          batch=self.batch)
+        self.clickables.append(
+            hud.Button('NEW GAME',
+                       x=SCREEN_WIDTH // 2,
+                       y=SCREEN_HEIGHT - 100,
+                       color=(255, 255, 255, 255),
+                       bg_color=(239, 68, 68),
+                       batch=self.batch,
+                       func=lambda: self.game.change_phase(IN_GAME)))
+        self.clickables.append(
+            hud.Button('LOAD SAVED',
+                       x=SCREEN_WIDTH // 2,
+                       y=SCREEN_HEIGHT - 200,
+                       color=(255, 255, 255, 255),
+                       bg_color=(239, 68, 68),
+                       batch=self.batch))
+        self.clickables.append(
+            hud.Button('LOAD QUICKSAVE',
+                       x=SCREEN_WIDTH // 2,
+                       y=SCREEN_HEIGHT - 300,
+                       color=(255, 255, 255, 255),
+                       bg_color=(239, 68, 68),
+                       batch=self.batch))
+
+    def on_draw(self):
+        self.game.window.clear()
+        self.batch.draw()
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        for clickable in self.clickables:
+            clickable.on_mouse_press(x, y, button, modifiers)
+
+
+class InGame(Phase):
+    def __init__(self, game):
+        super().__init__(game)
+        self.batch = pyglet.graphics.Batch()
+
+        pyglet.text.Label('IN GAME',
+                          color=(0, 0, 0, 255),
+                          anchor_x='center',
+                          x=SCREEN_WIDTH // 2,
+                          y=SCREEN_HEIGHT - 100,
+                          batch=self.batch)
+
+    def on_draw(self):
+        self.game.window.clear()
+        self.batch.draw()
+
+
+class PauseMenu(Phase):
+    def __init__(self, game):
+        super().__init__(game)
+        self.batch = pyglet.graphics.Batch()
+
+        pyglet.text.Label('PAUSED',
+                          color=(0, 0, 0, 255),
+                          anchor_x='center',
+                          x=SCREEN_WIDTH // 2,
+                          y=SCREEN_HEIGHT - 100,
+                          batch=self.batch)
 
 
 if __name__ == "__main__":
-    window = MainWindow(1280,
-                        720,
-                        "Post-Apocalyptic Survival Game",
-                        resizeable=False)
+    window = Game(SCREEN_WIDTH, SCREEN_HEIGHT,
+                  "Post-Apocalyptic Survival Game")
     pyglet.app.run()
